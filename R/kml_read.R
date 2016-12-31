41529b01408654afb4532ec1aaf107cb9f9d0628
François Briatte <briatte@gmail.com>
2016-12-31 16:30:38 +0100

    read KMZ file

--- R/kml_read.R
+++ R/kml_read.R
@@ -4,7 +4,10 @@
 #' All inputs accepted by \link[xml2:read_xml]{read_xml} are also accepted, as 
 #' long as they are valid KML. This means that URLs and local compressed files,
 #' such as \code{.zip} files, are also supported. 
-#' See \link[xml2:read_xml]{read_xml} for details.
+#' 
+#' If the source ends in \code{.kmz}, \code{kml_read} will treat it as a 
+#' \code{.zip} file and will try to read its first file using 
+#' \link[base:unz]{unz}.
 #' @param ... Arguments passed to \link[xml2:read_xml]{read_xml}, such as 
 #' \code{encoding} or \code{base_url}.
 #' See \link[xml2:read_xml]{read_xml} for details.
@@ -21,7 +24,14 @@
 #' @export
 kml_read <- function(x, ...) {
   
-  if (is.character(x)) {
+  if (is.character(x) && str_detect(x, "\\.kmz$")) {
+  
+    y <- unzip(x, list = TRUE)[ 1 ]
+    message("File: ", y)
+    
+    x <- read_xml(unz(x, y), ...)
+
+  } else if (is.character(x)) {
     x <- read_xml(x, ...)
   }
   
